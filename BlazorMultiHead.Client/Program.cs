0822c3589f304e066941ec2280ad82ddb6e2ba9d
Rockford Lhotka <rocky@lhotka.net>
2020-04-24 10:43:38 -0500

    Update for Blazor 3.2.0-preview5.20216.8

--- BlazorMultiHead.Client/Program.cs
+++ BlazorMultiHead.Client/Program.cs
@@ -2,21 +2,23 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using BlazorMultiHead.Client.Services;
+using System.Net.Http;
+using System;
 
 namespace BlazorMultiHead.Client
 {
-    public class Program
+  public class Program
+  {
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<Ui.App>("app");
+      var builder = WebAssemblyHostBuilder.CreateDefault(args);
+      builder.RootComponents.Add<Ui.App>("app");
 
-            builder.Services.AddBaseAddressHttpClient();
-            builder.Services.AddSingleton<Ui.Services.IWeatherForecastService, ForecastService>();
-            builder.Services.AddSingleton<Ui.Services.IHostType, HostType>();
+      builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+      builder.Services.AddSingleton<Ui.Services.IWeatherForecastService, ForecastService>();
+      builder.Services.AddSingleton<Ui.Services.IHostType, HostType>();
 
-            await builder.Build().RunAsync();
-        }
+      await builder.Build().RunAsync();
     }
+  }
 }
